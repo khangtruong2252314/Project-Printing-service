b2d7cb82e370dd0d36faefd0cb2e419a6610599e
khangtruong2252314 <khang.truong2252314@hcmut.edu.vn>
2024-11-17 22:27:46 +0700

    modified:   app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,6 @@
 
 module Main (main) where
 
-import Control.Monad.IO.Class (MonadIO(liftIO))
-import Data.Foldable (for_)
-import Data.IORef (modifyIORef, newIORef, readIORef)
-import Data.Map (Map)
-import qualified Data.Map as M
-import Data.Text (Text)
-import Text.Blaze.Html.Renderer.Text (renderHtml)
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
 import Web.Scotty
 
 import Lib 
@@ -20,36 +11,7 @@ main :: IO ()
 main = do
     database <- DB.init_database
     print_database <- DB.init_print_database
-    urlsR <- newIORef (1 :: Int, mempty :: Map Int Text)
     scotty 3000 $ do
-        get "/" $ do
-            (_, urls) <- liftIO $ readIORef urlsR
-            html $ renderHtml $
-                H.html $
-                H.body $ do
-                    H.h1 "Main"
-                    H.form H.! A.method "post" H.! A.action "/" $ do
-                        H.input H.! A.type_ "text" H.! A.name "url"
-                        H.input H.! A.type_ "submit"
-                        H.table $
-                            for_ (M.toList urls) $ \(i, url) ->
-                                H.tr $ do
-                                    H.td (H.toHtml i)
-                                    H.td (H.text url)
-        post "/" $ do
-            url <- formParam "url"
-            liftIO $ modifyIORef urlsR $
-                \(i, urls) ->
-                (i + 1, M.insert i url urls)
-            redirect "/"
-        get "/:n" $ do
-            n <- captureParam "n"
-            (_, urls) <- liftIO $ readIORef urlsR
-            case M.lookup n urls of
-                Just _ ->
-                    html "<h1> Countered! </h1>"
-                Nothing ->
-                    html $ renderHtml $ H.html $ H.body $ H.h1 "Not found"
         
         homeRoute  
         loginRoute
